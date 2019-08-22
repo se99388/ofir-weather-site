0a115ce8e9cf2e5eaca9a9f7d5cd8045c6522e84
ofir h <houbara0@gmail.com>
2019-08-23 02:02:53 +0300

    adding debounce to search.component.ts

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,26 +14,54 @@ export class SearchComponent implements OnInit {
     public searchValue: string;
     myControl = new FormControl();
     filteredOptions: City[];
-    constructor(private searchService: SearchService, private currentWeatherService:CurrentWeatherService) { }
+    public debounce: any;
+    constructor(private searchService: SearchService, private currentWeatherService: CurrentWeatherService) { }
 
     ngOnInit() {
+        this.debounce = this.debounceFn();
     }
 
     public search() {
-       
+       if (this.searchValue){
+        this.debounce(this.getApiSearch);
+       }     
+    }
+    
+    public debounceFn = () =>{
+        let idSetTimeout;
+        return function (getApi){
+            clearTimeout(idSetTimeout);
+            idSetTimeout = setTimeout(getApi,1000);
+        }
+    }  
+
+    public getApiSearch = ()=> {
+        console.log("this.searchValue",this.searchValue);
         this.searchService.getSearchByCity(this.searchValue).subscribe(res => {
             this.filteredOptions = res.map((item) => {
                 return {
                     name: item.LocalizedName + ", " + item.Country.LocalizedName,
                     id: item.Key
                 };
-
             });
             console.log(this.filteredOptions);
         });
     }
+    // public search() {
+    //     console.log("this.searchValue",this.searchValue);
+    //     this.searchService.getSearchByCity(this.searchValue).subscribe(res => {
+    //         this.filteredOptions = res.map((item) => {
+    //             return {
+    //                 name: item.LocalizedName + ", " + item.Country.LocalizedName,
+    //                 id: item.Key
+    //             };
 
-    public Selected(cityId:number,cityName:string):void {
+    //         });
+    //         console.log(this.filteredOptions);
+    //     });
+    // }
+
+    public Selected(cityId: number, cityName: string): void {
         console.log(cityId, cityName)
         this.currentWeatherService.addCurrentWeatherByCityId(cityId, cityName);
 
